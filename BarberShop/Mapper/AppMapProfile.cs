2fcfb7ed2b54c702e96a0d595afb3c0b4964fa03
Illya <illamatviuk@gmail.com>
2024-12-09 16:02:11 +0200

    Added mapper.

---
+++ BarberShop/Mapper/AppMapProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+using BarberShop.ViewModels.Country;
+using BarberShop.ViewModels.City;
+using BarberShop.Database.Entities;
+
+
+namespace BarberShop.Mapper
+{
+    public class AppMapProfile : Profile {
+        public AppMapProfile() {
+            CreateMap<Country, CountryVm>();
+            CreateMap<CreateCountryVm, Country>()
+                    .ForMember(c => c.Image, opt => opt.Ignore());
+        }
+    }
+}
