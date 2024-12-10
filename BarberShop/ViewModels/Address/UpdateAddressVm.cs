370f1bee3f144f43a2a8ae0ace58770fb69b23a7
Illya <illamatviuk@gmail.com>
2024-12-10 16:05:48 +0200

    Changed Address view model.

--- BarberShop/ViewModels/Address/UpdateAddressVm.cs
+++ BarberShop/ViewModels/Address/UpdateAddressVm.cs
@@ -2,6 +2,7 @@
 {
     public class UpdateAddressVm
     {
+        public long Id { get; set; }
         public string Street { get; set; } = null!;
 
         public string HouseNumber { get; set; } = null!;
