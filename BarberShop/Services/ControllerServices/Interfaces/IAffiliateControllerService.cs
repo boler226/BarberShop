7c4261f37d48f857fa6b04e6e47b94081ff1f6d3
Illya <illamatviuk@gmail.com>
2024-12-12 15:25:38 +0200

    Added Affiliate controller service.

---
+++ BarberShop/Services/ControllerServices/Interfaces/IAffiliateControllerService.cs
@@ -0,0 +1,11 @@
+﻿using BarberShop.ViewModels.Affiliate;
+
+namespace BarberShop.Services.ControllerServices.Interfaces
+{
+    public interface IAffiliateControllerService
+    {
+        Task CreateAsync(CreateAffiliateVm vm);
+        Task UpdateAsync(UpdateAffiliateVm vm);
+        Task DeleteIfExistsAsync(long id);
+    }
+}
