c3cd8820e6c0ce814c334e40b2ae302b4c25a3b5
Illya <illamatviuk@gmail.com>
2024-12-17 20:14:35 +0200

    Added User, Comment, Employee view models.

---
+++ BarberShop/ViewModels/Comment/CommentVm.cs
@@ -0,0 +1,14 @@
+﻿using BarberShop.ViewModels.Account;
+using BarberShop.ViewModels.Employee;
+
+namespace BarberShop.ViewModels.Comment
+{
+    public class CommentVm
+    {
+        public long Id { get; set; }
+        public int Rating { get; set; }
+        public string Description { get; set; } = null!;
+        public EmployeeVm Employee { get; set; } = null!;
+        public UserVm User { get; set; } = null!;
+    }
+}
---
+++ BarberShop/ViewModels/Employee/EmployeeVm.cs
@@ -0,0 +1,17 @@
+﻿using BarberShop.ViewModels.Comment;
+using BarberShop.ViewModels.Position;
+using BarberShop.ViewModels.Reservation;
+
+namespace BarberShop.ViewModels.Employee
+{
+    public class EmployeeVm
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Image { get; set; } = null!;
+        public int Rating { get; set; }
+        public PositionVm Position { get; set; } = null!;
+        public ICollection<CommentVm> Comments { get; set; } = null!;
+        public ICollection<ReservationVm> Reservations { get; set; } = null!;
+    }
+}
