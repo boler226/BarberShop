88462c73e79380f7a2a616f569eaca250ebbe7d9
Illya <illamatviuk@gmail.com>
2024-12-17 20:15:17 +0200

    Added Reservation view models.

---
+++ BarberShop/ViewModels/Reservation/CreateReservationVm.cs
@@ -0,0 +1,11 @@
+﻿namespace BarberShop.ViewModels.Reservation
+{
+    public class CreateReservationVm
+    {
+        public double Price { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public List<long> ServicesId { get; set; } = new List<long>();
+        public long UserId { get; set; }
+    }
+}
