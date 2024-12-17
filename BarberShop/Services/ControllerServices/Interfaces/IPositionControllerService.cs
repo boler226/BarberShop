ebf0fbdf1151e05ef950e309acc6e3f320acfe6c
Illya <illamatviuk@gmail.com>
2024-12-17 20:12:30 +0200

    Added Position controller service.

---
+++ BarberShop/Services/ControllerServices/Interfaces/IPositionControllerService.cs
@@ -0,0 +1,10 @@
+﻿using BarberShop.ViewModels.Position;
+
+namespace BarberShop.Services.ControllerServices.Interfaces
+{
+    public interface IPositionControllerService
+    {
+        Task CreateAsync(CreatePositionVm vm);
+        Task DeleteIfExistsAsync(long id);
+    }
+}
---
+++ BarberShop/Services/ControllerServices/PositionControllerService.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using BarberShop.Database.Context;
+using BarberShop.Database.Entities;
+using BarberShop.Services.ControllerServices.Interfaces;
+using BarberShop.ViewModels.Position;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarberShop.Services.ControllerServices
+{
+    public class PositionControllerService(
+         DataContext context,
+        IMapper mapper
+        ) : IPositionControllerService
+    {
+        public async Task CreateAsync(CreatePositionVm vm) {
+            var position = mapper.Map<Position>(vm);
+            
+            await context.Positions.AddAsync(position);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteIfExistsAsync(long id) { 
+            var position = await context.Positions.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (position is null)
+                return;
+
+            context.Positions.Remove(position);
+            await context.SaveChangesAsync();
+        }
+    }
+}
