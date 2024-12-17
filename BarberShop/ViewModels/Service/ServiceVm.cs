ebb490aeea592f26ace5bf1582840f42f3696663
Illya <illamatviuk@gmail.com>
2024-12-17 20:17:57 +0200

    Added Service view models.

---
+++ BarberShop/ViewModels/Service/ServiceVm.cs
@@ -0,0 +1,13 @@
+﻿using BarberShop.ViewModels.Reservation;
+
+namespace BarberShop.ViewModels.Service
+{
+    public class ServiceVm
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
+        public double Price { get; set; }
+        public double Time { get; set; }
+        public ReservationVm Reservation { get; set; }
+    }
+}
