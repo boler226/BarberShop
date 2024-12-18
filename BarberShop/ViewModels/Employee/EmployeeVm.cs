ccbb92393028724692c24c5fd74d0e7d721d9016
Illya <illamatviuk@gmail.com>
2024-12-18 17:56:38 +0200

    Added Employee view models.

--- BarberShop/ViewModels/Employee/EmployeeVm.cs
+++ BarberShop/ViewModels/Employee/EmployeeVm.cs
@@ -10,6 +10,7 @@ namespace BarberShop.ViewModels.Employee
         public string Name { get; set; } = null!;
         public string Image { get; set; } = null!;
         public int Rating { get; set; }
+        public long PositionId { get; set; }
         public PositionVm Position { get; set; } = null!;
         public ICollection<CommentVm> Comments { get; set; } = null!;
         public ICollection<ReservationVm> Reservations { get; set; } = null!;
---
+++ BarberShop/ViewModels/Employee/UpdateEmployeeVm.cs
@@ -0,0 +1,11 @@
+﻿namespace BarberShop.ViewModels.Employee
+{
+    public class UpdateEmployeeVm
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
+        public IFormFile Image { get; set; } = null!;
+        public int Rating { get; set; }
+        public int PositionId { get; set; }
+    }
+}
