f64c1285cf79881f09254caf48e98c3711acd419
Illya <illamatviuk@gmail.com>
2024-12-18 20:44:20 +0200

    Added Comment view models.

---
+++ BarberShop/ViewModels/Comment/CreateCommentVm.cs
@@ -0,0 +1,10 @@
+﻿namespace BarberShop.ViewModels.Comment
+{
+    public class CreateCommentVm
+    {
+        public int Rating { get; set; }
+        public string Description { get; set; } = null!;
+        public long EmployeeId { get; set; }
+        public long UserId { get; set; }
+    }
+}
