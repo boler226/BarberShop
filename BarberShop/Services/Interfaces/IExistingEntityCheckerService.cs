a9dc3fb45b5620bc76fcd4a5e2d95e5dcaebfe56
Illya <illamatviuk@gmail.com>
2024-12-22 17:54:36 +0200

    Added Existing entity service.

---
+++ BarberShop/Services/Interfaces/IExistingEntityCheckerService.cs
@@ -0,0 +1,16 @@
+﻿namespace BarberShop.Services.Interfaces
+{
+    public interface IExistingEntityCheckerService {
+        Task<bool> IsCorrectCountryId(long id, CancellationToken cancellationToken);
+        Task<bool> IsCorrectCityId(long id, CancellationToken cancellationToken);
+        Task<bool> IsCorrectAddressId(long id, CancellationToken cancellationToken);
+        Task<bool> IsCorrectAffiliateId(long id, CancellationToken cancellationToken);
+        Task<bool> IsCorrectBarbershopId(long id, CancellationToken cancellationToken);
+        Task<bool> IsCorrectCommentId(long id, CancellationToken cancellationToken);
+        Task<bool> IsCorrectEmployeeId(long id, CancellationToken cancellationToken);
+        Task<bool> IsCorrectPositionId(long id, CancellationToken cancellationToken);
+        Task<bool> IsCorrectReservationId(long id, CancellationToken cancellationToken);
+        Task<bool> IsCorrectServiceId(long id, CancellationToken cancellationToken); 
+        Task<bool> IsCorrectUserId(long id, CancellationToken cancellationToken);
+    }
+}
