c80175f363cca9c923b1d9cee14d96eb8bf607c6
Illya <illamatviuk@gmail.com>
2024-12-22 17:58:54 +0200

    Added Country validator.

--- BarberShop/Controllers/CountriesController.cs
+++ BarberShop/Controllers/CountriesController.cs
@@ -3,6 +3,7 @@ using AutoMapper.QueryableExtensions;
 using BarberShop.Database.Context;
 using BarberShop.Services.ControllerServices.Interfaces;
 using BarberShop.ViewModels.Country;
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,7 +14,9 @@ namespace BarberShop.Controllers
     public class CountriesController(
             DataContext context,
             IMapper mapper,
-            ICountriesControllerService service
+            ICountriesControllerService service,
+            IValidator<CreateCountryVm> createValidator,
+            IValidator<UpdateCountryVm> updateValidator
             ) : ControllerBase
     {
         [HttpGet]
@@ -39,13 +42,23 @@ namespace BarberShop.Controllers
 
         [HttpPost]
         public async Task<IActionResult> Create([FromForm] CreateCountryVm vm) { 
+            var validatorResult = await createValidator.ValidateAsync(vm);
+
+            if (!validatorResult.IsValid)
+                return BadRequest(validatorResult.Errors);
+
             await service.CreateAsync(vm);
 
             return Ok();
         }
 
         [HttpPut]
-        public async Task<IActionResult> Update([FromForm] UpdateCountryVm vm) { 
+        public async Task<IActionResult> Update([FromForm] UpdateCountryVm vm) {
+            var validatorResult = await updateValidator.ValidateAsync(vm);
+
+            if (!validatorResult.IsValid)
+                return BadRequest(validatorResult.Errors);
+
             await service.UpdateAsync(vm);
 
             return Ok();
