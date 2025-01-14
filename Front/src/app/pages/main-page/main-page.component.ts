bcc65859e15a7e5c2515cf90536c69f6b97e4b3d
Illya <illamatviuk@gmail.com>
2025-01-14 19:14:47 +0200

    Added video footage to main page.

--- Front/src/app/pages/main-page/main-page.component.ts
+++ Front/src/app/pages/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
@@ -7,6 +8,31 @@ import { Component } from '@angular/core';
   standalone: true,
   styleUrl: './main-page.component.scss'
 })
-export class MainPageComponent {
+export class MainPageComponent implements AfterViewInit {
+  @ViewChild('videoBackground') video!: ElementRef<HTMLVideoElement>
 
+  constructor() {
+
+  }
+
+  ngAfterViewInit(): void {
+    if (this.video.nativeElement) {
+      const videoElement = this.video.nativeElement
+      videoElement.muted = true
+      videoElement.disablePictureInPicture = true
+      videoElement.loop = false
+
+      videoElement.addEventListener('ended', () => {
+        console.log('Video ended')
+        videoElement.currentTime = 0
+        videoElement.play().catch(err => {
+          console.error('An error occurred while initializing video element.', err)
+        })
+      })
+
+      videoElement.play().catch(err => {
+        console.error('An error occurred while initializing video element.', err)
+      })
+    }
+  }
 }
