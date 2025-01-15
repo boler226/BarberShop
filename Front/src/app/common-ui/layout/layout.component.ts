a3ca03b99e9034a03d90c68d7f0efe690cfe7a52
Illya <illamatviuk@gmail.com>
2025-01-15 20:58:46 +0200

    Upgrade main page view and connected footer.

--- Front/src/app/common-ui/layout/layout.component.ts
+++ Front/src/app/common-ui/layout/layout.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from '../header/header.component';
+import {FooterComponent} from '../footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   imports: [
     RouterOutlet,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   templateUrl: './layout.component.html',
   standalone: true,
