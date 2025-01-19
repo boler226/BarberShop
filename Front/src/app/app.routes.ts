d88e2378528ed48bd88d870d49ac482f5bf46829
Illya <illamatviuk@gmail.com>
2025-01-19 20:17:56 +0200

    Added reservation page to router.

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -5,11 +5,13 @@ import {LayoutComponent} from './common-ui/layout/layout.component';
 import {canActivateAuth} from './auth/access.guard';
 import {MainPageComponent} from './pages/main-page/main-page.component';
 import {ContactPageComponent} from './pages/contact-page/contact-page.component';
+import {ReservationPageComponent} from './pages/reservation-page/reservation-page.component';
 
 export const routes: Routes = [
   {path: '', component: LayoutComponent, children: [
       {path: '', component: MainPageComponent},
-      {path: 'contacts', component: ContactPageComponent}
+      {path: 'contacts', component: ContactPageComponent},
+      {path: 'reservation', component: ReservationPageComponent},
     ],
     canActivate: [canActivateAuth]
   },
