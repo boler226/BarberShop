f3b3deaedc4b6ebf9ef2d9148352b94b5fef6321
Illya <illamatviuk@gmail.com>
2025-01-27 15:05:42 +0200

    Added JwtTokenRefresh view models.

--- BarberShop/ViewModels/Account/LoginVm.cs
+++ BarberShop/ViewModels/Account/LoginVm.cs
@@ -1,6 +1,6 @@
 ﻿namespace BarberShop.ViewModels.Account
 {
-    public class SignInVm {
+    public class LoginVm {
         public string Email { get; set; } = null!;
         public string Password { get; set; } = null!;
     }
---
+++ BarberShop/ViewModels/Account/RefreshTokenVm.cs
@@ -0,0 +1,7 @@
+﻿namespace BarberShop.ViewModels.Account {
+    public class RefreshTokenVm {
+        public string Token { get; set; } = null!;
+        public string RefreshToken { get; set; } = null!;
+
+    }
+}
