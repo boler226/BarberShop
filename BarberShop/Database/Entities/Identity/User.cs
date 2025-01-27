f2c923e0515c4150ca0d9fed38c7e04e892dbb0c
Illya <illamatviuk@gmail.com>
2025-01-27 15:06:48 +0200

    Changed UserEntity, added refresh token to it.

--- BarberShop/Database/Entities/Identity/User.cs
+++ BarberShop/Database/Entities/Identity/User.cs
@@ -7,6 +7,8 @@ namespace BarberShop.Database.Entities.Identity
         public string FirstName { get; set; } = null!;
         public string LastName { get; set; } = null!;
         public string Image {  get; set; } = null!;
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiry { get; set; }
         public virtual ICollection<UserRole> UserRoles { get; set; } = null!;
         public ICollection<Comment> Comments { get; set; } = new List<Comment>();
         public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
